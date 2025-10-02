33a0fc52ae401a639f451f16ca50eadd3c7569dd
kenersoncruz743-arch <kenersoncruz743@gmail.com>
2025-10-01 23:58:19 -0400

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,13 +1,15 @@
-// api/auth.js - VERSÃO CORRIGIDA
-const sheetsService = require('../lib/sheets');
-
-module.exports = async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
+// api/auth.js - Versão CORRIGIDA para ES Module
+  import sheetsService from '../lib/sheets'; //
+  
+  export default async function handler(req, res) { 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+  
   }
 
   if (req.method !== 'POST') {
