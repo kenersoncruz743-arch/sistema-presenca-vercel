501d145ddd080ff62704f95e61dc5aa29e7b9ec9
kenersoncruz743-arch <kenersoncruz743@gmail.com>
2025-10-05 21:07:07 -0400

    Update colaboradores.js

--- api/colaboradores.js
+++ api/colaboradores.js
@@ -1,4 +1,4 @@
-import sheetsService from '../lib/sheets'; // <-- CORRIGIDO
+import sheetsService from '../lib/sheets.js';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
